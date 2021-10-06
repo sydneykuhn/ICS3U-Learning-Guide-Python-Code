e614b76ed544ac57f6618491aa0bb58b48c138c8
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-06 12:06:32 -0400

    #1

--- code.py
+++ code.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Oct 2020
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+
+def main():
+    # this function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("The sum of just the positive numbers is = {0}".format(total))
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
