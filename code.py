7b50db349b13faf36cda6b8d2e1f9c632544cbdb
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-07 11:53:51 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,15 +8,21 @@ import ugame
 import stage
 
 
-def main():
+def game_scene():
     # this function is the main game game_scene
 
-    print("\n\n\n") # 3 blank lines
-    print("The sum of just the positive numbers is = {0}".format(total))
+    # image banks for CircuitPython 
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
 
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+    
     # repeat forever, game loop
     while True:
         pass # just a placeholder for now
 
 if __name__ == "__main__":
     game_scene()
+    
