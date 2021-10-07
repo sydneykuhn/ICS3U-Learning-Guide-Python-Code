26c852ae356d6c3c9818b1f7ce4de4a25d262924
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-07 12:29:08 -0400

    Update code.py

--- code.py
+++ code.py
@@ -13,15 +13,24 @@ def game_scene():
 
     # image banks for CircuitPython 
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    
     background = stage.Grid(image_bank_background, 10, 8)
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
     
     # repeat forever, game loop
     while True:
-        pass # just a placeholder for now
+        # get user input
+        
+        # update game logic
+        
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 if __name__ == "__main__":
     game_scene()
