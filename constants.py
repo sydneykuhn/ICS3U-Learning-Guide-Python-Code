b121850baa484a3266cf560589159eff421976f0
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-12 22:04:56 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Oct 2020
+# This program is the "Space Aliens" program on the PyBadge
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOUVMENT_SPEED = 1
