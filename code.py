b39925c1866a8a6ce4a94d8052cf85c560175547
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-12 21:13:38 -0400

    #4

--- code.py
+++ code.py
@@ -25,7 +25,25 @@ def game_scene():
     # repeat forever, game loop
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
         
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)    
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+            
         # update game logic
         
         # redraw Sprites
