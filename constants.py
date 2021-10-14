553220c28c99e48ba6699d39d37fc65356071f1d
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-14 16:42:00 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@
 # Created on: Oct 2020
 # This program is the "Space Aliens" program on the PyBadge
 
+# PyBadge screen size is 160x128 and sprites are 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -12,3 +13,11 @@ SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOUVMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
