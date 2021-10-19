a6fbd24a2564cce5c94c6159d21fbcdea51b7367
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-10-19 12:42:25 -0400

    #8

--- code.py
+++ code.py
@@ -6,9 +6,68 @@
 
 import ugame
 import stage
+import time
+import random
 
 import constants
 
+
+def splash_scene():
+    # this function is the splash scene
+
+    #get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+    # image banks for CircuitPython 
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+    # used this program to split the image into tile: 
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = [background]
+    game.render_block()
+    
+    # repeat forever, game loop
+    while True:
+        # wait for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
 
 def menu_scene():
     # this function is the main menu scene
@@ -67,7 +126,11 @@ def game_scene():
     sound.mute(False)
     
     background = stage.Grid(image_bank_background, 10, 8)
-    
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
+            
     ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
     
     alien = stage.Sprite(image_bank_sprites, 9,
@@ -122,11 +185,11 @@ def game_scene():
         # update game logic
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
-        
+
         # redraw Sprites
         game.render_sprites([ship] + [alien])
         game.tick()
 
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
     
